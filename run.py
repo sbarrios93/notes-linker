1030cf2c7c1500ad4ab6547889a01f353cb0ffa0
sbarrios93 <sbarrios93@gmail.com>
2021-07-01 19:12:43 -0400

    worked on run and markdownNote.py

--- run.py
+++ run.py
@@ -1,9 +1,26 @@
 from src.traverseFiles import getFilesPaths
 from src.markdownNote import MarkdownNote
 
+def loadFiles(path: str = 'notes', quiet: bool = True):
+    # load the name of markdown files and the path to each of them in a dict
+    filesDict = getFilesPaths(path)
+
+    # objectsDict will be the dictionary returned from the function, where key = normalized file name and value = MarkdownNote object
+    objectsDict = dict()
+    
+    # loop through each file and make a MarkdownNote object for each file
+    # then get the normalized file name from each object and make entry in dictionary
+    # where key is object's normalized name and value is the object
+    for file, filePath in filesDict.items():
+        mdNote = MarkdownNote(file, filePath, quiet = quiet)
+        # don't iterate if value of publish of the note is set to False or the note has no content
+        if mdNote.publish == True and mdNote.hasContent == True:
+            objectsDict[mdNote.filenameNormalized] = mdNote
+    return objectsDict
+
 # TODO: loop through objects and append which other files reference said object
-def loopSearchBackLinks() -> dict:
-    return
-
-# TODO: for each item in dictionary returned from getFilesPaths, create an object with MarkdownNote
-
+def loopSearchBackLinks(objects: dict) -> dict:
+    # generate a dict with keys of objects dict and empty lists as values
+    backlinks = {key: [] for key in objects.keys()}
+    for key, value in backlinks.items():
+        
--- src/markdownNote.py
+++ src/markdownNote.py
@@ -1,39 +1,90 @@
 import re
+import regex
 import os
+import warnings
+
 
 class MarkdownNote:
-    def __init__(self, filename: str, filePath: str, currentImageDir: str = "images",
-                 normalizedImageDir: str = "../img/post/"):
+    def __init__(
+        self,
+        filename: str,
+        filePath: str,
+        currentImageDir: str = "images",
+        normalizedImageDir: str = "../img/post/",
+        quiet: bool = True,
+    ):
         self.filename = filename
         self.filePath = filePath
         self.currentImageDir = currentImageDir
         self.normalizedImageDir = normalizedImageDir
+        self.quiet = quiet
         self.fileExtension = ".md"
         self.fileText = self._getText()
 
+        self.fileTitle = None 
+        self.fileWikiLinks = None 
+        self.fileWikiLinksText = None 
+        self.filenameNormalized = None 
+        self.fullCurrentImageDir = None 
+        self.fullNormalizedImageDir = None 
+
+
+        self.publish = False
 
         # regular expressions
-        self.regexHeaderOne = re.compile(
-            r"^(#[^#].*)",
-            re.MULTILINE
+        self.regexHeaderOne = regex.compile(
+            r"^(#[^#].*)", re.MULTILINE
         )  # this looks for the H1 headers in the file
-        self.regexWikiLink = re.compile(
-            r"(\[\[.+\]\])"
-        )  # this will look for all wiki-link-style strings (e.g. [[This is a WikiLink]] -> [[This is a WikiLink]])
-        self.regexWikiLinkText = re.compile(
-            r"\[\[(.+)\]\]"
-        )  # this will look for all wiki-link-style strings (e.g. [[This is a WikiLink]] -> This is a WikiLink)
-        self.regexFrontMatter = re.compile(r"^---[\s\S]+?---")
 
-        # functions
-        self.fileTitle = self._getTitle()
-        self.fileWikiLinks = self._getWikiLinks()
-        self.fileWikiLinksText = self._getWikiLinksText()
-        self.filenameNormalized = self._normalizeFilename()
-        self.fullCurrentImageDir = self._getCurrentImageDir()
-        self.fullNormalizedImageDir = self._getNormalizedImageDir()
+        self.regexWikiLink = regex.compile(
+            r"(\[\[.+?\]\])"
+        )  # this will look for all wiki-link-style strings (e.g. [[This is a WikiLink]] -> [[This is a WikiLink]])
+
+        self.regexWikiLinkText = regex.compile(
+            r"\[\[(.+?)\]\]"
+        )  # this will look for all wiki-link-style strings (e.g. [[This is a WikiLink]] -> This is a WikiLink)
+
+        self.regexWikiLinkSentences = lambda _wikiLink : regex.compile(fr"[^.?!\n]*(?<=[.?\s!\n])\[\[{_wikiLink}\]\].*?(?=[\s.?!\n])[^.?!\n]*[.?!\n]") # this lambda function lets you input a wikilink (without the brackets and get all the surrounding text (the sentence where that wikilink exists.))
+
+        self.regexFrontMatter = regex.compile(
+            r"^---[\s\S]+?---"
+        )  # find the frontmatter element
+
+        self.regexContentPostFrontMatter = regex.compile(
+            r"^---[\s\S]+?---\K[\s\S]*\S[\s\S]*", flags=regex.DOTALL
+        )  # find everything that there is after a front matter.
+
+        self.regexContentNoFrontMatter = regex.compile(
+            r"[\s\S]*\S[\s\S]*", flags=regex.DOTALL
+        )  # find everything that there is when there's no front matter
+
+        # get structure of note
         self.frontMatterDict = self._passFrontMatter()
+        self.hasContent = self._checkHasContent()
+
+        # before running the below functions we need to check if the file has any content
+        # (besides the frontmatter)
+        # functions
+        if self.hasContent:
+            self.fileTitle = self._getTitle()
+            self.fileWikiLinks = self._getWikiLinks()
+            self.fileWikiLinksText = self._getWikiLinksText()
+            self.fileWikiLinksSentences = self._getWikiLinksSentences()
+            self.filenameNormalized = self._normalizeFilename()
+            self.fullCurrentImageDir = self._getCurrentImageDir()
+            self.fullNormalizedImageDir = self._getNormalizedImageDir()
+        else:
+            # if quiet is set to true, show warning when file has no content besides frontmatter
+            if not self.quiet:
+                warnings.warn(f"File {filename} ({filePath}) has no content.")
         
+
+        # modify type of value in self.publish
+        if not isinstance(self.publish, bool):
+            if (self.publish == "True") or (self.publish == "true"):
+                self.publish = True
+            else: 
+                self.publish = False
 
     def _getText(self) -> str:
         with open(self.filePath, "r", encoding="utf-8") as file:
@@ -44,10 +95,18 @@ class MarkdownNote:
         return re.sub(r"^#[ ]*", "", firstHeaderOne)
 
     def _getWikiLinks(self) -> list:
-        return self.regexWikiLink.findall(self.fileText)
-        
+        return list(set(self.regexWikiLink.findall(self.fileText)))
+
     def _getWikiLinksText(self) -> list:
-        return self.regexWikiLinkText.findall(self.fileText)
+        return list(set(self.regexWikiLinkText.findall(self.fileText)))
+
+    def _getWikiLinksSentences(self) -> dict:
+        sentencesDict = dict()
+        for link in self.fileWikiLinksText:
+            regex_ = self.regexWikiLinkSentences(link)
+            sentencesDict[link] =  regex_.findall(self.fileText)
+        return sentencesDict
+            
 
     def _normalizeFilename(self) -> str:
         return self.filename.lower().replace(" ", "-")
@@ -59,20 +118,48 @@ class MarkdownNote:
         return os.path.join(self.normalizedImageDir, self.filenameNormalized)
 
     def _passFrontMatter(self) -> dict:
+        # init frontMatterDict (this is the returned dict of this function)
+        frontMatterDict = dict()
         frontMatter = self.regexFrontMatter.findall(self.fileText)
-        if not frontMatter:
-            raise RuntimeError("No frontmatter detected")
-        # clean frontmatter, remove '---', remove whitespace and
-        # return dictionary with key, value for each attribute in frontmatter
-        frontMatter = frontMatter[0].replace('---', '').split('\n')
-        frontMatter = [re.sub(r"^\s+|\s+$", "", a) for a in frontMatter if a]
-        frontMatterDict = {re.search(r"(^[a-zA-Z0-9]*)[^:]", i).group(0): re.search(r":[ ]*(.*)\s*$", i).group(1) for i in frontMatter}
 
-        assert "publish" in frontMatterDict, "There should be at least a key 'publish' in the front matter"
-        # assign keys of dictionary to attributes of the class
+        # set an attribute if frontmatter exists in the file or not
+        # if it doesn't exists, set a single value in returned dictionary
+        # where publish is false.
+        if not frontMatter:
+            self.frontMatterExists = False
+            # the value publish in the markdown is True or False with type string, so we use the same type to pass the value in the dictionary
+            frontMatterDict = {"publish": "False"}
+            if not self.quiet:
+                warnings.warn(
+                    f"Front matter NOT DETECTED in file {self.filename} ({self.filePath}), the 'publish' value will be set to 'False'"
+                )
+        else:
+            self.frontMatterExists = True
+            # clean frontmatter, remove '---', remove whitespace and
+            # return dictionary with key, value for each attribute in frontmatter
+            frontMatter = frontMatter[0].replace("---", "").split("\n")
+            frontMatter = [re.sub(r"^\s+|\s+$", "", a) for a in frontMatter if a]
+            frontMatterDict = {
+                re.search(r"(^[a-zA-Z0-9]*)[^:]", i)
+                .group(0): re.search(r":[ ]*(.*)\s*$", i)
+                .group(1)
+                for i in frontMatter
+            }
+
+            # if "publish" not a key in the dictionary (wasn't in the frontmatter), set it by default to false, print warning.
+            if "publish" not in frontMatterDict and not self.quiet:
+                warnings.warn(
+                    f"Front matter in file {self.filename} ({self.filePath})detected but didn't have key 'publish', so it has been set as 'False' by default"
+                )
+                # the value publish in the markdown is True or False with type string, so we use the same type to pass the value in the dictionary
+                frontMatterDict["publish"] = "False"
+            # assign keys of dictionary to attributes of the class
         for key in frontMatterDict:
             setattr(self, key, frontMatterDict[key])
-        
+
         return frontMatterDict
 
-
+    def _checkHasContent(self):
+        if self.frontMatterExists:
+            return True if self.regexContentPostFrontMatter.findall(self.fileText) else False
+        else: return True if self.regexContentNoFrontMatter.findall(self.fileText) else False
