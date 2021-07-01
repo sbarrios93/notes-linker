e4200c6bc9c8bc6c18955136d7e927969f6c455a
sbarrios93 <sbarrios93@gmail.com>
2021-06-30 22:51:09 -0400

    added TODOS for run.pu

---
+++ run.py
@@ -0,0 +1,9 @@
+from src.traverseFiles import getFilesPaths
+from src.markdownNote import MarkdownNote
+
+# TODO: loop through objects and append which other files reference said object
+def loopSearchBackLinks() -> dict:
+    return
+
+# TODO: for each item in dictionary returned from getFilesPaths, create an object with MarkdownNote
+
